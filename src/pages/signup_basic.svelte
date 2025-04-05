c86c86453779f6b0da03bbc7abedfc79249e1306
Ezra Abarca <39611157+ezrillex@users.noreply.github.com>
2025-04-04 18:22:25 -0600

    feat/ form prototype

--- src/pages/signup_basic.svelte
+++ src/pages/signup_basic.svelte
@@ -1,18 +1,35 @@
-<script>
+<script lang="ts">
     import {push} from 'svelte-spa-router'
-    function enviarDatos(){
+
+    let year = 2024;
+    let duinit = '';
+    let email = '';
+
+    async function enviarDatos(){
         console.log("Enviando...");
+        await fetch("http://localhost:3000/monitors/create",{
+            method:"POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                year: year,
+                duinit: duinit,
+                email: email
+            })
+        })
+        console.log("Enviado!")
         push('/cuenta');
     }
 </script>
 
 <main>
     <p>Year to monitor</p>
-    <input value="2024" type="number" step="1" min="2020" max="2025" />
+    <input bind:value={year} type="number" step="1" min="2020" max="2025" />
     <p>Your DUI or NIT</p>
-    <input type="text"/>
+    <input bind:value={duinit} type="text"/>
     <p>Email to notify</p>
-    <input type="email"/>
+    <input bind:value={email} type="email"/>
     <button on:click={enviarDatos}>Submit</button>
 </main>
 
